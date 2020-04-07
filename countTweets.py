13f74abe6d3c925f2bc7a920ddabb3c33ff776e5
sbelke <63214748+sbelke@users.noreply.github.com>
2020-04-06 22:04:01 -0400

    Update countTweets.py

---
+++ countTweets.py
@@ -0,0 +1,7 @@
+def countTweets(json):
+  """ keeps track of the number of tweets that come in
+  
+  args: json - json file that is being inputed
+        count - previous count
+        
+   returns : int - number of total tweets"""
