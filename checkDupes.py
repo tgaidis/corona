e7a8392b3a4f5a3a8e456ccada91300ad85e3359
sbelke <63214748+sbelke@users.noreply.github.com>
2020-04-06 21:59:16 -0400

    Update checkDupes.py

---
+++ checkDupes.py
@@ -0,0 +1,8 @@
+def checkDupes():
+  """check the unique id of all the tweets to make sure that we are not plotting the same tweet twice
+    Args:
+        jsonf : json file
+        
+    Returns:
+        boolean : True if there is a duplicate
+    """
