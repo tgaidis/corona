d092cf250f6b7b667087b18df5b640041ce9534b
sbelke <63214748+sbelke@users.noreply.github.com>
2020-05-17 15:35:30 -0400

    Update countTweets.py

--- countTweets.py
+++ countTweets.py
@@ -1,7 +1,11 @@
-def countTweets(json):
-  """ keeps track of the number of tweets that come in
-  
-  args: json - json file that is being inputed
-        count - previous count
-        
-   returns : int - number of total tweets"""
+def countTweets(dupdic, total):
+    dickeys = dupdic.keys()
+    number_of_keys = len(dickeys)
+    dicvalues = dupdic.values()
+    dupes = sum(dicvalues)
+    dupes_number = dupes - number_of_keys
+    final = total - dupes_number
+    return(final)
+    
+    
+    
