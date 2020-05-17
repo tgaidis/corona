6d269bd9baa717295cbe41a81d41c037dae20d62
sbelke <63214748+sbelke@users.noreply.github.com>
2020-05-17 15:36:28 -0400

    Update checkDupes.py

--- checkDupes.py
+++ checkDupes.py
@@ -1,8 +1,18 @@
-def checkDupes():
-  """check the unique id of all the tweets to make sure that we are not plotting the same tweet twice
-    Args:
-        jsonf : json file
-        
-    Returns:
-        boolean : True if there is a duplicate
-    """
+def checkDupes(filepath):
+    tidl = []
+    tiddic = dict()
+    f = open(filepath, "r")
+    for cat in f:
+        cat_strip = cat.strip("\n")
+        cat_split = cat_strip.split(",")
+        tid = cat_split[1]
+        tidl.append(tid)
+    number_of_tweets = len(tidl)
+    for item in tidl:
+        if item in tiddic:
+            tiddic[item] += 1
+        else:
+            tiddic[item] = 1
+    tiddic = {key:value for key, value in tiddic.items() if value > 1}
+    return tiddic, number_of_tweets    
+    
